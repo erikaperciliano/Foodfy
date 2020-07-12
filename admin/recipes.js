b83dcdafcc43b51ee0b6be9878c378c607226bbc
Erika Perciliano <erikaperciliano.developer@gmail.com>
2020-07-12 13:29:40 -0300

    some adjustments

--- admin/recipes.js
+++ admin/recipes.js
@@ -3,11 +3,15 @@ const data = require('../data');
 
 
 
-/*exports.index = function(req, res){
-    return res.render('instructors/index', {instructors: data.instructors});
-}*/
 
 exports.index = function(req, res){
     return res.render('admin/index', {admin: data.index});
 }
 
+exports.create = function(req, res){
+    return res.render("admin/create", {admin: data.create}); 
+}
+
+exports.post = function(req, res){
+    return res.send('Recebido!');
+}
