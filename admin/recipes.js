88f13f583430800af45a9c68e7a0320fb4b3d14e
Erika Perciliano <erikaperciliano.developer@gmail.com>
2020-08-06 23:03:18 -0300

    add update

--- admin/recipes.js
+++ admin/recipes.js
@@ -6,7 +6,7 @@ const data = require('../data.json');
 
 
 exports.index = function(req, res){
-    return res.render('admin/index', {admin: data.recipes});
+    return res.render('admin/index', {recipes: data.recipes});
 }
 
 exports.create = function(req, res){
